a716e4b0a743bcd2cfc53dc6341dba0aa7798977
deshpandeaparajita <aparajita.deshpande1991@gmail.com>
2021-03-29 15:22:13 +1100

    added test cases

--- progress-bar/src/components/IncrementButtonComponent/IncrementButtonComponent.test.jsx
+++ progress-bar/src/components/IncrementButtonComponent/IncrementButtonComponent.test.jsx
@@ -5,8 +5,31 @@ import IncrementButtonComponent from './IncrementButtonComponent';
 
 
 describe('IncrementButtonComponent', () => {
+    const props = {
+        handleClick:jest.fn(),
+        buttons: [12, 2, -13],
+    };
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<IncrementButtonComponent {...props} />);
+    }
+    );
+
     it('should match snapshot', () => {
-        const component = shallow(<IncrementButtonComponent />);
-        expect(component).toMatchSnapshot();
+        expect(wrapper).toMatchSnapshot();
     });
+
+    it('verify text for first button', () => {
+        const first_button_text = wrapper.find('button').first().text();
+        expect(first_button_text).toBe('Increment by 12');
+    })
+    it('verify text for second button', () => {
+        const first_button_text = wrapper.find('button').at(1).text();
+        expect(first_button_text).toBe('Increment by 2');
+    })
+    it('verify text for third button', () => {
+        const first_button_text = wrapper.find('button').at(2).text();
+        expect(first_button_text).toBe('Increment by -13');
+    })
+    
 });
--- progress-bar/src/components/PercentageComponent/PercentageComponent.test.jsx
+++ progress-bar/src/components/PercentageComponent/PercentageComponent.test.jsx
@@ -5,8 +5,23 @@ import PercentageComponent from './PercentageComponent';
 
 
 describe('PercentageComponent', () => {
+    const props = {
+        percentRange: 20,
+    };
+
     it('should match snapshot', () => {
         const component = shallow(<PercentageComponent />);
         expect(component).toMatchSnapshot();
+    });
+    it('verify background color when range is within limit', () => {
+        const wrapper = shallow(<PercentageComponent {...props} />);
+        expect(wrapper.find('.completed').prop('style')).toHaveProperty('background', 'limegreen');
+    });
+    it('verify background color is red when range reached limit', () => {
+        const props = {
+            percentRange: 100,
+        };
+        const wrapper = shallow(<PercentageComponent {...props} />);
+        expect(wrapper.find('.completed').prop('style')).toHaveProperty('background', 'red');
     });
 });
